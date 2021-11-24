d188711127f95688829445725f03c5a3aa14e854
yipower <84766762+yipower@users.noreply.github.com>
2021-11-24 18:52:57 +0800

    Add files via upload

---
+++ 商业app/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<view class="">
+			shoye
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ 商业app/pages/news/news.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<view class="">
+			资讯
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
